352bce3874b6042e5165238cf295849511088279
osx11 <osx11@ya.ru>
2022-07-06 17:48:42 +0300

    test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
 	import Portfolio from "../lib/Portfolio.svelte";
 	import Quotes from "../lib/Quotes.svelte";
 
-	import {scrollToAnchor} from '../anchor_handler';
+	import {scrollToAnchor} from '../anchor_handler.ts';
 </script>
 
 <head>
